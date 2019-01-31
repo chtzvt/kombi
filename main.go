50a2b6fb9e8278aed76eb3a819b0b1949faada9c
Charlton Trezevant <ct@ctis.me>
2019-01-31 00:26:45 -0500

    Update main.go to leverage the shiny new linkserver package.

--- main.go
+++ main.go
@@ -1,67 +1,56 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"kombi/db"
+	"kombi/linkserver"
 	"log"
-	"net/http"
 	"time"
 )
 
-var linkDb = &db.CachedSqliteDb {
-	FilePath:               "./links.db",
-	CacheDefaultExpiration: 5*time.Minute,
-	CacheExpiredPurgeTime:  5*time.Minute,
-	Initialize:             false,
-}
-
 func main() {
-	// Echo instance
 	e := echo.New()
 
 	// Middleware
 	e.Use(middleware.Logger())
-	//e.Use(middleware.Recover())
+	e.Use(middleware.Recover())
 
+	linkDb := &db.CachedSqliteDb{
+		FilePath:               "./links.db",
+		CacheDefaultExpiration: 5 * time.Minute,
+		CacheExpiredPurgeTime:  5 * time.Minute,
+		Initialize:             false,
+	}
 
 	if err := db.CachedSqliteDatabase(linkDb); err != nil {
-		log.Fatal("Error creating database: ", err.Error())
+		log.Fatal(fmt.Sprintf("Couldn't configure database: ", err.Error()))
 	}
 
-	// Routes
-	e.GET("/", LinkServer)
-	//e.GET("/settings", Settings)
-	//e.GET("/api", API)
+	linkserver.RegisterDatabase(linkDb)
+	linkserver.RegisterPathHandlers(e)
 
-	e.GET("/:link", LinkServer)
+	//e.GET("/kombi", Settings)
+	//e.POST("/kombi/api", API)
 
-	time.Sleep(2*time.Second)
+	/*
+		if err := linkDb.CreateLink("", "https://www.hackucf.org/"); err != nil {
+			log.Printf("Error adding link to database: %s", err.Error())
+		}
 
-	if err := linkDb.CreateLink("", "https://www.hackucf.org/"); err != nil {
-		log.Printf("Error adding link to database: %s", err.Error())
-	}
+		if err := linkDb.CreateLink("ct2", "http://chill.ctis.me/"); err != nil {
+			log.Printf("Error adding link to database: %s", err.Error())
+		}
 
-	if err := linkDb.CreateLink("ct2", "http://chill.ctis.me/"); err != nil {
-		log.Printf("Error adding link to database: %s", err.Error())
-	}
+		if err := linkDb.CreateLink("ct", "https://www.ctis.me/"); err != nil {
+			log.Printf("Error adding link to database: %s", err.Error())
+		}
 
-	if err := linkDb.CreateLink("ct", "https://www.ctis.me/"); err != nil {
-		log.Printf("Error adding link to database: %s", err.Error())
-	}
+		if err := linkDb.DeleteLink("ct2"); err != nil {
+			log.Printf("Error removing link from database: %s", err.Error())
+		}
+	*/
 
-	if err := linkDb.DeleteLink("ct2"); err != nil {
-		log.Printf("Error removing link from database: %s", err.Error())
-	}
-
-	// Start server
-	e.Logger.Fatal(e.Start(":80"))
-}
-
-func LinkServer(c echo.Context) error {
-	if dest, err := linkDb.GetLinkDestination(c.Param("link")); err != nil {
-		return c.Redirect(http.StatusFound, "/")
-	} else {
-		return c.Redirect(http.StatusFound, dest)
-	}
+	e.Logger.Fatal(e.Start(":8080"))
 }
